fda6624c05c96bd7af289d9901440d4158d981c1
Timur <abkeevtimur@gmail.com>
2025-06-09 18:29:03 +0300

    build the app

--- src/api.ts
+++
@@ -1,5 +0,0 @@
-export const fetchUsers = async () => {
-  const response = await fetch('http://localhost:5000/api/users');
-  if (!response.ok) throw new Error('Network error');
-  return await response.json();
-};
